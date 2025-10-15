410c562aefb18a6f5dc6baa5371a9bad345030c8
carlosDevSanchez <c.sanchez@finerik.com>
2025-10-15 09:12:33 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'foundation-blue': '#2A4B9B',
+        'foundation-pink': '#E74699',
+        'foundation-primary': '#E74699',
+        'foundation-green': '#8DAF7A',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
