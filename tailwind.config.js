fbe8158a4fb2590b497280f78562414e1147cacf
carlosDevSanchez <carlosdevsanchez@gmail.com>
2026-01-14 00:46:42 -0500

    feat: add new design canvas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      title: ['Poppins', 'sans-serif'],
+      body: ['Inter', 'sans-serif'],
+    },
     extend: {
       colors: {
         'foundation-blue': '#2A4B9B',
         'foundation-pink': '#E74699',
         'foundation-primary': '#E74699',
         'foundation-green': '#8DAF7A',
-      },
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
       },
     },
   },
