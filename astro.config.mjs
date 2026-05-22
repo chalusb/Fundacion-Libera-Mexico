944ca659b57c43c9a0f47a40d734c45999b13503
chalusb <chaluuf@gamil.com>
2026-05-22 13:28:39 -0600

    numero y dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,8 @@ export default defineConfig({
         'fundacion-libera-mexico-production.up.railway.app',
         'liberamexico.org',
         'fundacionliberamexico.org',
+        'liberamexico.com',
+        'www.liberamexico.com'
       ],
     },
   },
